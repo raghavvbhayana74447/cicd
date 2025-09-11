234f7aa3666c674b690e94530f861f3e59ff3e9f
raghavvbhayana74447 <raghavbhayana.kainskep@gmail.com>
2025-09-11 14:18:38 +0530

    minor changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hi</h1>"
+    return "<h1>Hi this page is hosted by ci/cd pipeline</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)

9f75b7054b349d66b4b3411a1537c87875d1e5d3
raghavvbhayana74447 <raghavbhayana.kainskep@gmail.com>
2025-09-11 14:23:15 +0530

    changing port

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.11-slim
 WORKDIR /home/raghav/jenkins/workspace/pythonapp
 COPY main.py .
-EXPOSE 8000
+EXPOSE 8001
 RUN pip install flask
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def home():
     return "<h1>Hi this page is hosted by ci/cd pipeline</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8001)
